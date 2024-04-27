e3e4b3cfc531acfcdf3b20f81310eb17d2566431
PiusLim373 <piuslim373@gmail.com>
2024-04-28 02:12:36 +0800

    Added gripper and basic pick and place

--- src/me5415_moveit_config/src/robot_controller.py
+++ src/me5415_moveit_config/src/robot_controller.py
@@ -4,7 +4,12 @@ import sys
 import moveit_commander
 import moveit_msgs.msg
 import copy
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, Point
+from me5415_moveit_config.srv import *
+from std_srvs.srv import SetBool, SetBoolResponse
+from std_msgs.msg import Float64
+import time
+import math
 
 
 class MoveGroupPythonInteface(object):
@@ -19,29 +24,10 @@ class MoveGroupPythonInteface(object):
         display_trajectory_publisher = rospy.Publisher(
             "/move_group/display_planned_path", moveit_msgs.msg.DisplayTrajectory, queue_size=20
         )
-
-        print("done declaring pub")
-
         planning_frame = move_group.get_planning_frame()
-        print("============ Planning frame: %s" % planning_frame)
-
-        # We can also print the name of the end-effector link for this group:
         eef_link = move_group.get_end_effector_link()
-        print("============ End effector link: %s" % eef_link)
-
-        # We can get a list of all the groups in the robot:
         group_names = robot.get_group_names()
-        print("============ Available Planning Groups:", robot.get_group_names())
 
-        # Sometimes for debugging it is useful to print the entire state of the
-        # robot:
-        print("============ Printing robot state")
-        print(robot.get_current_state())
-        print("")
-        ## END_SUB_TUTORIAL
-
-        # Misc variables
-        self.box_name = ""
         self.robot = robot
         self.scene = scene
         self.move_group = move_group
@@ -49,6 +35,86 @@ class MoveGroupPythonInteface(object):
         self.planning_frame = planning_frame
         self.eef_link = eef_link
         self.group_names = group_names
+
+        self.gripper_opening_pub = rospy.Publisher(
+            "left_finger_joint_position_controller/command", Float64, queue_size=1
+        )
+        # rospy.Service("go_to_saved_joint", GoToSavedJoint, self.go_to_saved_joint_cb)
+        # rospy.Service("move_with_joint", MoveWithJoint, self.move_with_joint_cb)
+        # rospy.Service("move_with_cartesian", MoveWithCartesian, self.move_with_cartesian_cb)
+        # rospy.Service("move_in_cartesian_direction", MoveInCartesianDirection, self.move_in_cartesian_direction_cb)
+        # rospy.Service("get_robot_states", GetRobotStates, self.get_robot_states_cb)
+        # rospy.Service("gripper_activation", SetBool, self.gripper_activation_cb)
+        rospy.loginfo("Initialization Completed!")
+
+    def go_to_saved_joint_cb(self, req):
+        res = GoToSavedJointResponse()
+        res.success = self.go_to_saved_joint(req.saved_joint)
+        return res
+
+    def move_with_joint_cb(self, req):
+        res = MoveWithJointResponse()
+        if len(req.joint_angles) != 6:
+            rospy.logerr("6 joint angles are expected!")
+            res.success = False
+            return res
+        res.success = self.move_with_joint(req.joint_angles)
+        return res
+
+    def move_with_cartesian_cb(self, req):
+        res = MoveWithCartesianResponse()
+        print(req)
+        res.success = self.move_with_cartesian(req.pose_goal.pose)
+        return res
+
+    def move_in_cartesian_direction_cb(self, req):
+        res = MoveInCartesianDirectionResponse()
+        if not req.direction in ["x", "y", "z"]:
+            rospy.logerr("Only x, y and z direction is allowed!")
+            res.success = False
+            return res
+        if req.distance == 0.0:
+            rospy.logerr("distance cant be zero")
+            res.success = False
+            return res
+        res.success = self.move_in_cartesian_direction(req.direction, req.distance)
+        return res
+
+    def get_robot_states_cb(self, req):
+        print("====== joint states ======")
+        print(self.robot.get_current_state())
+        print("====== cartesian states ======")
+        print(self.move_group.get_current_pose())
+        res = GetRobotStatesResponse()
+        res.joint_angles = list(self.robot.get_current_state().joint_state.position)
+        res.pose = self.move_group.get_current_pose().pose
+        res.success = True
+        return res
+
+    def gripper_activation_cb(self, req):
+        # set true to close, false to open
+        res = SetBoolResponse()
+        data = Float64()
+        if req.data:
+            self.open_gripper()
+        else:
+            self.close_gripper()
+        res.success = True
+        return res
+
+    def open_gripper(self, opening=0.03):
+        data = Float64()
+        data.data = opening
+        self.gripper_opening_pub.publish(data)
+        time.sleep(1)
+        return True
+
+    def close_gripper(self, opening=0.00):
+        data = Float64()
+        data.data = opening
+        self.gripper_opening_pub.publish(data)
+        time.sleep(1)
+        return True
 
     def go_to_saved_joint(self, saved_joint):
         joint_goal = self.move_group.set_named_target(saved_joint)
@@ -61,12 +127,80 @@ class MoveGroupPythonInteface(object):
         self.move_group.stop()
         return result
 
-    def move_with_cartesian(self, pose_goal):
-        self.move_group.set_pose_target(pose_goal)
-        result = self.move_group.go(wait=True)
+    def generate_waypoints_with_step(self, start_pose, end_pose, use_curr_quaternion=False, distance_per_step=0.05):
+
+        step_size = int(
+            max(
+                [
+                    math.ceil(abs(end_pose.position.x - start_pose.position.x) / distance_per_step),
+                    math.ceil(abs(end_pose.position.y - start_pose.position.y) / distance_per_step),
+                    math.ceil(abs(end_pose.position.z - start_pose.position.z) / distance_per_step),
+                ]
+            )
+        )
+        waypoints = []
+
+        incremental = Point(
+            (end_pose.position.x - start_pose.position.x) / step_size,
+            (end_pose.position.y - start_pose.position.y) / step_size,
+            (end_pose.position.z - start_pose.position.z) / step_size,
+        )
+        print(end_pose)
+        for i in range(1, step_size + 1):
+            temp_waypoint = Pose()
+            temp_waypoint.position.x = start_pose.position.x + i * incremental.x
+            temp_waypoint.position.y = start_pose.position.y + i * incremental.y
+            temp_waypoint.position.z = start_pose.position.z + i * incremental.z
+            if use_curr_quaternion:
+                temp_waypoint.orientation.x = start_pose.orientation.x
+                temp_waypoint.orientation.y = start_pose.orientation.y
+                temp_waypoint.orientation.z = start_pose.orientation.z
+                temp_waypoint.orientation.w = start_pose.orientation.w
+            else:
+                temp_waypoint.orientation.x = end_pose.orientation.x
+                temp_waypoint.orientation.y = end_pose.orientation.y
+                temp_waypoint.orientation.z = end_pose.orientation.z
+                temp_waypoint.orientation.w = end_pose.orientation.w
+            waypoints.append(temp_waypoint)
+
+        return waypoints
+
+    def move_with_cartesian(self, pose_goal_list):
+        use_curr_quaternion = False
+        if len(pose_goal_list) == 3:
+            use_curr_quaternion = True
+        pose_goal = self.list_to_pose(pose_goal_list)
+        print(self.move_group.get_current_pose().pose)
+        pose_list = self.generate_waypoints_with_step(
+            start_pose=self.move_group.get_current_pose().pose,
+            end_pose=pose_goal,
+            use_curr_quaternion=use_curr_quaternion,
+        )
+        for x in pose_list:
+            print(x.position.x, x.position.y, x.position.z)
+        (path, frac) = self.move_group.compute_cartesian_path(pose_list, 0.01, 0.0)
+        print(f"frac: {frac}")
+        result = self.move_group.execute(path, wait=True)
         self.move_group.stop()
-        self.move_group.clear_pose_targets()
         return result
+
+        # for i in range(10):
+        #     (traj, frac) = self.move_group.compute_cartesian_path(pose_list, 0.01, 0.0)
+        #     print(f"frac: {frac}")
+        #     # print(traj)
+        #     shoulder_pan_start = traj.joint_trajectory.points[0].positions[0]
+        #     shoulder_pan_final = traj.joint_trajectory.points[-1].positions[0]
+        #     if abs(shoulder_pan_final - shoulder_pan_start) > 0.5 * math.pi:
+        #         rospy.logwarn("Joint flip detected, to retry")
+        #         continue
+        #     else:
+        #         print("inside go")
+        #         result = self.move_group.execute(traj, wait=True)
+        #         self.move_group.stop()
+        #         print(result)
+        #         return result
+        # rospy.logwarn("All 10 trial result in joint flip")
+        # return False
 
     def move_cartesian_direction(self, axis, distance):
         waypoints = []
@@ -92,41 +226,102 @@ class MoveGroupPythonInteface(object):
         print("====== cartesian states ======")
         print(self.move_group.get_current_pose())
 
+    def list_to_pose(self, data):
+        temp = Pose()
+        temp.position.x = data[0]
+        temp.position.y = data[1]
+        temp.position.z = data[2]
+        if len(data) != 7:
+            temp.orientation = self.move_group.get_current_pose().pose.orientation
+        else:
+            temp.orientation.x = data[3]
+            temp.orientation.y = data[4]
+            temp.orientation.z = data[5]
+            temp.orientation.w = data[6]
+        return temp
 
-def main():
+
+def transfer_meatball():
+    print("source table homing")
+    if ROBOT.go_to_saved_joint("source_home"):
+        print("moving to meatball tray")
+        if ROBOT.move_with_cartesian(ITEM_POSES['meatball']):
+            print("opening gripper")
+            ROBOT.open_gripper()
+            print("going down")
+            if ROBOT.move_cartesian_direction("z", -0.1):
+                print("closing gripper")
+                ROBOT.close_gripper()
+                print("going up")
+                if ROBOT.move_cartesian_direction("z", 0.1):
+                    print("source table homing")
+                    if ROBOT.go_to_saved_joint("source_home"):
+                        print("assemble table homing")
+                        if ROBOT.go_to_saved_joint("assemble_home"):
+                            print("moving to plate")
+                            if ROBOT.move_with_cartesian(ITEM_POSES['plate']):
+                                print("going down")
+                                if ROBOT.move_cartesian_direction("z", -0.1):
+                                    print("opening gripper")
+                                    ROBOT.open_gripper()
+                                    print("going up")
+                                    if ROBOT.move_cartesian_direction("z", 0.1):
+                                        print("closing gripper")
+                                        ROBOT.close_gripper()
+                                        print("assemble table homing")
+                                        if ROBOT.go_to_saved_joint("assemble_home"):
+                                            print("all done!")
+
+def transfer_brocolli():
+    print("source table homing")
+    if ROBOT.go_to_saved_joint("source_home"):
+        print("moving to brocolli tray")
+        if ROBOT.move_with_cartesian(ITEM_POSES['brocolli']):
+            print("opening gripper")
+            ROBOT.open_gripper()
+            print("going down")
+            if ROBOT.move_cartesian_direction("z", -0.1):
+                print("closing gripper")
+                ROBOT.close_gripper()
+                print("going up")
+                if ROBOT.move_cartesian_direction("z", 0.1):
+                    print("source table homing")
+                    if ROBOT.go_to_saved_joint("source_home"):
+                        print("assemble table homing")
+                        if ROBOT.go_to_saved_joint("assemble_home"):
+                            print("moving to plate")
+                            if ROBOT.move_with_cartesian(ITEM_POSES['plate']):
+                                print("going down")
+                                if ROBOT.move_cartesian_direction("z", -0.1):
+                                    print("opening gripper")
+                                    ROBOT.open_gripper()
+                                    print("going up")
+                                    if ROBOT.move_cartesian_direction("z", 0.1):
+                                        print("closing gripper")
+                                        ROBOT.close_gripper()
+                                        print("assemble table homing")
+                                        if ROBOT.go_to_saved_joint("assemble_home"):
+                                            print("all done!")
+
+def debug():
+    pass
+
+def run_service():
     rospy.init_node("robot_controller")
     robot = MoveGroupPythonInteface()
+    rospy.spin()
 
-    input("press enter go to source table via saved joint goal")
-    robot.go_to_saved_joint("source_home")
-
-    input("press enter go to assembly table via saved joint goal")
-    robot.go_to_saved_joint("assemble_home")
-
-    input("press enter to move via joint")
-    robot.move_with_joint([1.0, -2.5, 1, -1, -1.5, 0])  # 6 joint value in radian
-
-    input("press enter to move via joint")
-    robot.move_with_joint([0.75, -2, 2, -1.5, -1.5, 0])  # 6 joint value in radian
-
-    print(robot.get_status())
-    input("press enter to move via cartesian")  # supply a pose, use ik to calculate the joint angle and move
-    pose_goal = Pose()
-    pose_goal.orientation.x = 1
-    pose_goal.orientation.y = 0
-    pose_goal.orientation.z = 0
-    pose_goal.orientation.w = 0
-    pose_goal.position.x = 0.2
-    pose_goal.position.y = 0.15
-    pose_goal.position.z = 1.0
-    robot.move_with_cartesian(pose_goal)
-
-    input("press enter to move via in specific cartesian direction")
-    robot.move_cartesian_direction("z", -0.1)  # move in a specific cartesian direction, wrt to base_link
-
-    input("press enter to move via in specific cartesian direction")
-    robot.move_cartesian_direction("x", 0.2)  # move in a specific cartesian direction, wrt to base_link
-
+def transfer_food_flow():
+    transfer_brocolli()
+    transfer_meatball()
 
 if __name__ == "__main__":
-    main()
+    rospy.init_node("robot_controller")
+    ITEM_POSES = rospy.get_param("~poses", None)
+    print(ITEM_POSES)
+    ROBOT = MoveGroupPythonInteface()
+    transfer_food_flow()
+
+
+# grab juice joint
+# -2.099098391718041, -0.8816061889314906, 1.643638716631668, 5.507127109864094, 5.768607540901943, -1.5504155977946583, 0.00445721352188873, 0.004457190463231838
